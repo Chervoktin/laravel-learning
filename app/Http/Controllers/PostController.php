43be81f3d79657335b2816e1ecb41e25683449e7
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-05 17:33:28 +0300

    add PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use IPostRepository;
+use Post;
+
+class PostController extends Controller
+{
+
+    public function messages()
+    {
+        return [
+            'text.required' => 'Необходимо заполнить поле',
+        ];
+    }
+
+    public function add(Request $request, IPostRepository $repository)
+    {
+        if (Auth::check()) {
+            $posts = $repository->getAll();
+            return view('addPost', ['posts' => $posts]);
+        } else {
+            return redirect('login');
+        }
+    }
+
+    public function addComplite(Request $request, IPostRepository $repository)
+    {
+        if (Auth::check()) {
+            $rules = [
+                'text' => 'max:255|required',
+                'title' => 'required|max:50',
+            ];
+            $messages = [
+                'text.required' => 'Необходимо заполнить текст',
+                'title.required' => 'Необходимо заполнить заголовок',
+            ];
+            $this->validate($request, $rules, $messages);
+            $post = new Post($request->input('title'), $request->input('text'));
+            $repository->save($post);
+            return redirect('blog');
+        } else {
+            return redirect('login');
+        }
+    }
+}
