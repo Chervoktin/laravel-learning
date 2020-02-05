098a4c25d36cef37d91fa5f1332db4251640fbce
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-05 17:42:27 +0300

    Revert "add Repositories\ArraRepository.php"
    This reverts commit e7e572dd8c0ff55b704439515fc6a9cc1cd9bfff.

--- app/Contracts/IPostRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare (strict_types = 1);
-
-interface IPostRepository
-{
-    public function findById(int $id): Post;
-    public function getAll(): array;
-    public function save(Post $post): void;
-}
