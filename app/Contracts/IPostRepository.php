0706fd45392193d9116b8c203674c90011c4e273
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-06 00:45:56 +0300

    change PostRepository

--- app/Contracts/IPostRepository.php
+++ app/Contracts/IPostRepository.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 interface IPostRepository
 {
     public function findById(int $id): Post;
-    public function getAll(): array;
+    public function getAllByUserId(int $id): array;
     public function save(Post $post): void;
+    public function deleteByIdAndUserId(int $id, int $user_id): void;
 }
