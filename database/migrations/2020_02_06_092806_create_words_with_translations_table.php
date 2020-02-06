4fff4fa1915de2ff654c49a364cec68df309c18a
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-06 12:43:37 +0300

    Create 2020_02_06_092806_create_words_with_translations_table.php

---
+++ database/migrations/2020_02_06_092806_create_words_with_translations_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsWithTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words_with_translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('word_id')->unsigned();
+            $table
+                ->foreign('word_id')
+                ->references('id')
+                ->on('words')
+                ->onDelete('cascade');
+            $table->integer('translation_id')->unsigned();
+            $table
+                ->foreign('translation_id')
+                ->references('id')
+                ->on('translation')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words_with_translations');
+    }
+}
