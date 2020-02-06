b91a6b7e0e697931aa5732992e45601b3cd6cef1
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-06 19:20:52 +0300

    Create CardRepository.php

---
+++ app/Repositories/CardRepository.php
@@ -0,0 +1,28 @@
+<?php
+declare (strict_types = 1);
+use Illuminate\Support\Facades\DB;
+class CardNotFoundException extends Exception{
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
+
+class CardRepository implements ICardRepository
+{
+
+    public function save($card)
+    {
+        return DB::table('cards')->insertGetId(
+            array('text' => $card->text)
+        );
+    }
+    public function findById($id)
+    {
+        $arr = DB::select('select * from cards where id = ?', [$id]);
+        if(isset($arr[0])){
+            return $arr[0];
+        }else{
+            throw new CardNotFoundException("card with id: " . $id . " not found");
+        }
+    }
+}
