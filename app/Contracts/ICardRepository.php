58b7cd9af20667d7549cf43452d78901a7154c13
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-09 22:09:22 +0300

    Update ICardRepository.php

--- app/Contracts/ICardRepository.php
+++ app/Contracts/ICardRepository.php
@@ -1,6 +1,10 @@
 <?php
 
-interface ICardRepository{
+interface ICardRepository {
+
     public function save($card);
+
     public function findById($id);
+
+    public function addWordWithTranslation($card_id, $word_with_translation_id);
 }
