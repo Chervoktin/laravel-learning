c65f9e8d3990c0a1f68e2fe24fdb77907abeab07
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-10 22:32:06 +0300

    Update 2020_02_06_093715_create_cards_with_words_with_translations_table.php

--- database/migrations/2020_02_06_093715_create_cards_with_words_with_translations_table.php
+++ database/migrations/2020_02_06_093715_create_cards_with_words_with_translations_table.php
@@ -4,28 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCardsWithWordsWithTranslationsTable extends Migration
-{
+class CreateCardsWithWordsWithTranslationsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('cards_with_words_with_translations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
             $table->integer('words_with_translations_id')->unsigned();
             $table
-                ->foreign('words_with_translations_id')
-                ->references('id')
-                ->on('words_with_translations');
+                    ->foreign('words_with_translations_id')
+                    ->references('id')
+                    ->on('words_with_translations')
+                    ->onDelete('cascade');
             $table->integer('card_id')->unsigned();
             $table
                     ->foreign('card_id')
                     ->references('id')
-                    ->on('cards');
+                    ->on('cards')
+                    ->onDelete('cascade');
         });
     }
 
@@ -34,8 +35,8 @@ class CreateCardsWithWordsWithTranslationsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('cards_with_words_with_translations');
     }
+
 }
