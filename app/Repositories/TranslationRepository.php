831b5f93e095ab1a7e29846e6495b3856532917d
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-12 15:22:20 +0300

    Update TranslationRepository.php

--- app/Repositories/TranslationRepository.php
+++ app/Repositories/TranslationRepository.php
@@ -1,11 +1,31 @@
 <?php
+
 use Illuminate\Support\Facades\DB;
+
+class TranslationNotFoundException extends Exception {
+
+    public function __construct($message, $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
 
 class TranslationRepository implements ITranslationRepository {
 
     public function save($translation): int {
         return DB::table('translations')->insertGetId(
                         array('translation' => $translation->translation));
+    }
+
+    public function findTranslation(string $translation) {
+        $sql = 'select * from translations where translations.translation = ?';
+        $bindings = [$translation];
+        $translations = DB::select($sql, $bindings);
+        if (isset($translations[0])) {
+            return $translations[0];
+        } else {
+            throw new TranslationNotFoundException('Translation ' . $translation . ' not found');
+        }
     }
 
 }
