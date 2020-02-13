71d581f1141944c15c996842cb72bdf83da112e7
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-13 16:37:12 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,6 +4,11 @@
     <title>
         добавление карточки
     </title>
+    <style>
+        .complite {
+            background-color: greenyellow;
+        }
+    </style>
     <script>
         async function getWordsByCardId(cardId) {
             let response = await fetch('/words/' + cardId);
@@ -28,7 +33,7 @@
                 let p = document.createElement('span');
                 p.innerHTML = word.word + "   :";
                 input.id = word.id;
-                input.value = word.translation;
+                input.oninput = translationComparison;
                 let div = document.getElementById("test");
                 div.append(p);
                 div.append(input);
@@ -36,11 +41,34 @@
         }
 
         function input(event){
-            console.log(event.target.value);
+            let card = window.cards[currentCardPosition-1];
+            if(event.target.value == card.text){
+                createTest(card.words);
+                event.target.classList.add("complite");
+            }
+        }
+
+        function translationComparison(event){
+            let card = window.cards[window.currentCardPosition-1];
+            let words = card.words;
+            let id = event.target.id;
+            let word = null;
+            for(let i = 0; i != words.length; i++){
+                if(words[i].id == id){
+                    word = words[i];
+                }
+            }
+            if(word.translation == event.target.value){
+                event.target.classList.add("complite");
+            }
+
         }
 
         document.addEventListener("DOMContentLoaded", async function () {
             let cards = await getCards();
+            for(let i = 0; i!=cards.length; i++){
+                cards[i].words = await getWordsByCardId(cards[i].id);
+            }
             window.cards = cards;
             window.currentCardPosition = 1;
             window.AllCards = cards.length;
@@ -50,24 +78,17 @@
         });
 
         async function next() {
-            document.getElementById("prev").disabled = true;
             window.currentCardPosition += 1;
-            let words = await getWordsByCardId(window.cards[window.currentCardPosition - 1].id);
-            document.getElementById("prev").disabled = false;
             let div = document.getElementById("test");
             while (div.lastChild != null) {
                 div.removeChild(div.lastChild);
             }
-            
             let positionDiv = document.getElementById("position");
             positionDiv.innerHTML = window.currentCardPosition + "/" + window.AllCards;
             document.getElementById("text").innerHTML = window.cards[window.currentCardPosition - 1].text;
         }
         async function prev() {
-            document.getElementById("prev").disabled = true;
             window.currentCardPosition -= 1;
-            let words = await getWordsByCardId(window.cards[window.currentCardPosition - 1].id);
-            document.getElementById("prev").disabled = false;
             let div = document.getElementById("test");
             while (div.lastChild != null) {
                 div.removeChild(div.lastChild);
