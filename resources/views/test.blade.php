e16b948fde14977470cc645e55091efcb8d88946
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-13 15:52:50 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -15,13 +15,67 @@
                 alert("Ошибка HTTP: " + response.status);
             }
         }
-        async function getCards(){
+        async function getCards() {
             let response = await fetch('/cards/');
-            let json =  await response.json();
+            let json = await response.json();
             return json;
         };
-        document.addEventListener("DOMContentLoaded", function () {
+
+        function createTest(words) {
+            for (let i = 0; i != words.length; i++) {
+                let word = words[i];
+                let input = document.createElement('input');
+                let p = document.createElement('span');
+                p.innerHTML = word.word + "   :";
+                input.id = word.id;
+                input.value = word.translation;
+                let div = document.getElementById("test");
+                div.append(p);
+                div.append(input);
+            }
+        }
+
+        function input(event){
+            console.log(event.target.value);
+        }
+
+        document.addEventListener("DOMContentLoaded", async function () {
+            let cards = await getCards();
+            window.cards = cards;
+            window.currentCardPosition = 1;
+            window.AllCards = cards.length;
+            let positionDiv = document.getElementById("position");
+            positionDiv.innerHTML = window.currentCardPosition + "/" + window.AllCards;
+            document.getElementById("text").innerHTML = cards[window.currentCardPosition - 1].text;
         });
+
+        async function next() {
+            document.getElementById("prev").disabled = true;
+            window.currentCardPosition += 1;
+            let words = await getWordsByCardId(window.cards[window.currentCardPosition - 1].id);
+            document.getElementById("prev").disabled = false;
+            let div = document.getElementById("test");
+            while (div.lastChild != null) {
+                div.removeChild(div.lastChild);
+            }
+            
+            let positionDiv = document.getElementById("position");
+            positionDiv.innerHTML = window.currentCardPosition + "/" + window.AllCards;
+            document.getElementById("text").innerHTML = window.cards[window.currentCardPosition - 1].text;
+        }
+        async function prev() {
+            document.getElementById("prev").disabled = true;
+            window.currentCardPosition -= 1;
+            let words = await getWordsByCardId(window.cards[window.currentCardPosition - 1].id);
+            document.getElementById("prev").disabled = false;
+            let div = document.getElementById("test");
+            while (div.lastChild != null) {
+                div.removeChild(div.lastChild);
+            }
+            let positionDiv = document.getElementById("position");
+            positionDiv.innerHTML = window.currentCardPosition + "/" + window.AllCards;
+            document.getElementById("text").innerHTML = window.cards[window.currentCardPosition - 1].text;
+        }
 
     </script>
 
@@ -29,7 +83,14 @@
 
 <body>
 
+    <div id="text">
+    </div>
+    <input type="text" oninput="input(event)"></input>
+    <div id="position"></div>
+    <button onclick="prev()" id="prev"> prev </button> <button onclick="next()" id="next"> next </button>
+    <div id="test">
 
+    </div>
 </body>
 
 </html>
