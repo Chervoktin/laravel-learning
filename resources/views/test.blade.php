535fa05260e897f48a38da029fbf2495d6d1a91f
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-13 19:45:19 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -31,42 +31,55 @@
                 let word = words[i];
                 let input = document.createElement('input');
                 let p = document.createElement('span');
+                let br = document.createElement('br');
                 p.innerHTML = word.word + "   :";
                 input.id = word.id;
                 input.oninput = translationComparison;
                 let div = document.getElementById("test");
                 div.append(p);
                 div.append(input);
+                div.append(br);
             }
         }
 
-        function input(event){
-            let card = window.cards[currentCardPosition-1];
-            if(event.target.value == card.text){
+        function input(event) {
+            let card = window.cards[window.currentCardPosition - 1];
+            if (event.target.value == card.text) {
                 createTest(card.words);
                 event.target.classList.add("complite");
             }
         }
 
-        function translationComparison(event){
-            let card = window.cards[window.currentCardPosition-1];
+        async function translationComparison(event) {
+            let card = window.cards[window.currentCardPosition - 1];
             let words = card.words;
             let id = event.target.id;
             let word = null;
-            for(let i = 0; i != words.length; i++){
-                if(words[i].id == id){
+            for (let i = 0; i != words.length; i++) {
+                if (words[i].id == id) {
                     word = words[i];
                 }
             }
-            if(word.translation == event.target.value){
+            if (word.translation == event.target.value) {
                 event.target.classList.add("complite");
+                let response = await fetch("/increment/" + word.id);
+                console.log(word.word + " : " + await response.text());
+                response = await fetch("/incrementcard/" + card.id);
+                console.log(card.text + " : " + await response.text());
+                window.countOfRightWords += 1;
+                if (window.countOfRightWords == words.length) {
+                    window.countOfRightWords = 0;
+                    next();
+                    
+                }
             }
 
         }
 
         document.addEventListener("DOMContentLoaded", async function () {
+            window.countOfRightWords = 0;
             let cards = await getCards();
-            for(let i = 0; i!=cards.length; i++){
+            for (let i = 0; i != cards.length; i++) {
                 cards[i].words = await getWordsByCardId(cards[i].id);
             }
             window.cards = cards;
