94eb2e22c98f7f81cdec5cefe093084464291a23
Kirill <58602653+Chervoktin@users.noreply.github.com>
2020-02-26 19:35:18 +0300

    Update 2020_02_06_091738_create_cards_table.php

--- database/migrations/2020_02_06_091738_create_cards_table.php
+++ database/migrations/2020_02_06_091738_create_cards_table.php
@@ -17,6 +17,7 @@ class CreateCardsTable extends Migration
             $table->bigIncrements('id');
             $table->integer('scores')->unsigned()->default(0);
             $table->string('text');
+            $table->string('url');
             $table->timestamps();
         });
     }
